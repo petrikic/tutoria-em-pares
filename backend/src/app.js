61fb11b5574460e8ba7a537480e4ed66ec43adfe
RenatoShishido <renato.re2012@hotmail.com>
2020-03-17 00:22:20 -0400

    version .10 backend
    socket io implemented.
    missinging:
    conection online,
    add id socket in model users,
    export to a ROUTE especif,
    load history of chat.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const path = require('path')
+var socket = require('socket.io');
+
+
+// Static files
+// app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile('index.html', {root : __dirname + '/public'})
+})
 
 
 require("./database/index")
+
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
@@ -14,9 +24,44 @@ require('./app/controllers/index')(app)
 
 
 
+const PORT = 3333
+var server = app.listen(PORT, '192.168.0.107',
+() => console.log(`Servidor rodando na porta: ${PORT}`))
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`))
+// Sockeat IO
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
